1360b9f0dc45b01f15dbe622f62e2b66611b706c
SHANJAY RK <126944929+SHANJAYRK@users.noreply.github.com>
2024-02-29 10:14:06 +0530

    Add files via upload

---
+++ Day 4/script.js
@@ -0,0 +1,9 @@
+var light = document.getElementById("light");
+function toggleLight(values){
+    if(values){
+        light.className = "lighton"
+    }
+    else{
+        light.className = "lightoff"
+    }
+}
